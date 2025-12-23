001c20bda05b12322baa97fed4eca6695f84e49e
fouad lmrini <fouad11.lamrini@gmail.com>
2025-12-23 15:29:18 +0100

    ajouter eslint

---
+++ back-end/src/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', 
+  ],
+  rules: {
+    semi: ['error', 'always'],       
+    quotes: ['error', 'single'],       
+    indent: ['error', 2],           
+    'prettier/prettier': 'error',
+  },
+};
